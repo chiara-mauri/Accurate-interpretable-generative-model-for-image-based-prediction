fa4dea7bd2206922189d44039e52f597392932be
chiara-mauri <131471021+chiara-mauri@users.noreply.github.com>
2023-12-08 17:47:44 -0500

    Add files via upload

---
+++ Python_code/runTraining.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 11:49:48 2023
+
+@author: cm1991
+"""
+
+import scipy as sp
+import numpy as np
+import hdf5storage
+import pickle
+import argparse
+import matplotlib.pyplot as plt
+
+from trainModel import *
+
+
+parser = argparse.ArgumentParser(description="Script for training the model",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+def list_of_ints(arg):
+    return list(map(int, arg.split(',')))
+
+#parser.add_argument("-nLatentValues", required =  True, nargs="+", help="list with number of latent variables to try, e.g. -nLatentValues 20,50,100")
+parser.add_argument("-nLatentValues", required =  True, type=list_of_ints, help="list of number of latent variables to try, e.g. -nLatentValues 20,50,100")
+parser.add_argument("-sp","--savePath", help="path where the trained model is saved", default="./")
+parser.add_argument("-n","--nameSavedModel", help="name given to trained model",default="trainedModel")
+parser.add_argument("-dp","--dataPath", help="path to data", default="./")
+parser.add_argument("-th","--maskThreshold", type=float ,help="threshold for data mask", default=0.01)
+
+
+args = parser.parse_args()
+config = vars(args)
+print(config)
+
+nLatentValues = config['nLatentValues']
+saveModelPath = config['savePath']
+nameSavedModel = config['nameSavedModel']
+dataPath = config['dataPath']
+maskThreshold = config['maskThreshold']
+
+
+showFigures = True
+
+#load training data
+
+age = np.load(dataPath + "trainAge_N1000.npy")
+# array of size (# of subjects, 1) with age of all training subjects
+allVolumes = np.load(dataPath + "trainImages_N1000.npy")
+# 4D array of size (# of subjects, image dimension 1, image dimension 2, image dimension 3)
+# with images of all training subjects
+
+targetTrain = age
+N_train = len(targetTrain)
+covariatesTrain = np.empty((N_train,0))
+
+
+#train basis functions
+trainBasisFunInit = np.concatenate((targetTrain,covariatesTrain),axis=1)
+nBasisFun = np.shape(trainBasisFunInit)[1] #number of basis functions
+  
+
+trainBasisFunMean = np.mean(trainBasisFunInit,axis=0) #vector 1 x nBasisFun with means
+trainBasisFunStd = np.std(trainBasisFunInit,axis=0) #vector 1 x nBasisFun with stds
+trainBasisFun = (trainBasisFunInit-trainBasisFunMean)/trainBasisFunStd
+ 
+
+   #%%
+
+#compute mask of voxels 
+avgVol=np.squeeze(np.mean(allVolumes,axis=0))
+avgVolScaled=avgVol/np.max(avgVol)
+indecesMask = (avgVolScaled>maskThreshold)
+        
+if showFigures:
+    slice1 = avgVol[:,:,30]
+    plt.imshow(slice1, cmap='gray')
+    plt.colorbar()
+    plt.show()
+
+    slice2 = avgVolScaled[:,:,30]
+    plt.imshow(slice2, cmap='gray')
+    plt.colorbar()
+    plt.show()
+
+    mask = np.zeros(np.shape(avgVol))
+    mask[indecesMask] = 1
+    slice3 = mask[:,:,30]
+    plt.imshow(slice3, cmap='gray')
+    plt.colorbar()
+    plt.show()
+#%%
+
+
+trainImagesInit=np.zeros((N_train,np.sum(indecesMask)))
+for n in np.arange(N_train):
+    vol = np.squeeze(allVolumes[n,:,:,:])
+    trainImagesInit[n,:] = np.transpose(vol[indecesMask])
+del allVolumes
+      
+
+imTrainMean = np.mean(trainImagesInit,axis=0) 
+imTrainStd = np.std(trainImagesInit,axis=0)
+   
+trainImages=(trainImagesInit-imTrainMean)/imTrainStd 
+del trainImagesInit
+
+
+#initializations
+nLatentTot = len(nLatentValues)
+nIterationsAll = np.zeros([nLatentTot,1])
+timeTrainingAll = np.zeros([nLatentTot,1])
+Vall = list()
+betaAll = list()
+logmlVectorAll = list()
+
+#model training 
+
+n = 0
+for nLatentVars in nLatentValues:
+    
+    print("N. latent = " + str(nLatentVars) + "\n")
+    (W, V, beta, timeTraining, logmlVector, nIterations) = trainModel(trainImages,trainBasisFun,nLatentVars)
+
+
+    nIterationsAll[n] = nIterations
+    Vall.append(V)
+    betaAll.append(beta)
+    timeTrainingAll[n] = timeTraining
+    logmlVectorAll.append(logmlVector)
+     
+    n = n+1
+
+
+#compute original weights (without standardization) and visualize them
+
+W_original_0 = np.multiply(np.multiply(np.reshape(imTrainStd,[np.sum(indecesMask),1]),W),np.reshape(1/trainBasisFunStd,[1,nBasisFun]))
+W_original_offset = np.reshape(imTrainMean,[np.sum(indecesMask),1])
+W_original = np.concatenate((W_original_0, W_original_offset),axis=1)
+#%%
+if showFigures:
+    #target weights
+    sliceIndex = 30
+    weights3D = np.zeros(np.shape(avgVol))
+    weights3D[indecesMask] = W_original[:,0]
+    sliceW = weights3D[:,:,sliceIndex]
+    alphas = np.abs(sliceW)
+    alphas = alphas/np.max(alphas)
+    template = avgVol[:,:,sliceIndex]
+    alphasTemplate = np.zeros(np.shape(avgVol))
+    alphasTemplate[indecesMask] = 0.5
+    plt.imshow(template, alpha = alphasTemplate[:,:,sliceIndex], cmap='gray')
+    plt.imshow(sliceW, alpha = alphas)
+    colorbar = plt.colorbar()
+    #plt.colorbar.set_alpha(0.5)
+    plt.title('target weights')
+    plt.show()
+    
+    #offset
+    offset3D = np.zeros(np.shape(avgVol))
+    offset3D[indecesMask] = W_original[:,-1]
+    slice_offset = offset3D[:,:,sliceIndex]
+    plt.imshow(slice_offset,cmap='gray')
+    colorbar = plt.colorbar()
+    #plt.colorbar.set_alpha(0.5)
+    plt.title('offset')
+    plt.show()
+    
+    
+#%%
+model = {
+  "W": W,
+  "W_original": W_original,
+  "Vall": Vall,
+  "betaAll": betaAll,
+  "timeTrainingAll": timeTrainingAll,
+  "logmlVectorAll": logmlVectorAll,
+  "nIterationsAll": nIterationsAll,
+  "trainBasisFunMean": trainBasisFunMean,
+  "trainBasisFunStd": trainBasisFunStd,
+  "imTrainMean": imTrainMean,
+  "imTrainStd": imTrainStd, 
+  "indecesMask": indecesMask,
+  "nLatentValues": nLatentValues 
+}
+
+ 
+#save trained model
+modelPklFile = saveModelPath + nameSavedModel + ".pkl"  
+
+with open(modelPklFile, 'wb') as file:  
+    pickle.dump(model, file)
---
+++ Python_code/trainModel.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 17:00:29 2022
+
+@author: chiara
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 20 17:06:01 2022
+
+@author: cmau
+"""
+
+import numpy as np
+import scipy as sp
+import time
+
+
+def trainModel(trainImages,trainBasisFun,nLatentVars,costTol=1e-5, nItMin=10, nItMax=500):
+    
+    #costTol is tolerance for convergence of logml in the training
+    #nItMin is minimum number of iterations during training 
+
+
+
+    #compute causal weights W  
+    
+    D_W = np.matmul(np.transpose(trainImages),trainBasisFun)
+    A_W = np.matmul(np.transpose(trainBasisFun),trainBasisFun)
+    W = np.transpose(np.linalg.solve(np.transpose(A_W),np.transpose(D_W)))
+    #w_star = W[:,1]
+          
+    nVoxels = np.shape(W)[0]
+    nBasisFun = np.shape(W)[1]
+    Ntrain = np.shape(trainBasisFun)[0]
+          
+    #inItialize parameters beta and V
+        
+    betaInit = np.zeros((nVoxels,))
+    nLoops = np.ceil(nVoxels/20000)
+    for i in np.arange(nLoops):
+        indeces = np.arange(i*20000,np.amin([(i+1)*20000,nVoxels]),dtype=int)
+        betaInit[indeces] = np.transpose(1/(np.var(trainImages[:,indeces],0))) 
+        
+    beta = np.reshape(betaInit,(nVoxels,1))
+        
+    #rng(n_inIt)
+        
+       
+    VInit = np.random.normal(size=(nVoxels,nLatentVars)) #randomly sampled from standard Gaussian
+    V = VInit
+     
+        
+     
+    logML = float('inf')
+    logML_old = float('inf')
+        
+        
+    logmlVector = np.zeros([nItMax,1])    
+      
+    
+       
+    
+    #start training
+    t = time.time()
+
+
+    for nIt in np.arange(nItMax):
+        if nIt > nItMin:
+            if (np.abs((logML - logML_old)/logML) < costTol):  
+                V = Vold
+                beta = betaOld
+                
+                break
+         
+        
+        print('it number:', nIt, '\n')
+    
+        #save old values
+        Vold = V
+        betaOld = beta
+        logML_old = logML
+          
+        #save logML values for each iteration
+        logmlVector[nIt] = logML
+           
+        #diagBetas = sp.sparse.spdiags(beta,0,nVoxels,nVoxels)
+  
+        
+        
+        #update of posterior mean and variance of latent variables
+    
+        #SigmaZ_inv=np.eye(nLatentVars)+np.linalg.multi_dot((np.transpose(V),diagBetas,V))
+        SigmaZ_inv=np.eye(nLatentVars)+np.dot(np.transpose(V),beta*V)
+        SigmaZ=np.linalg.inv(SigmaZ_inv)
+            
+        dimBlock = 8000
+        nBlocksTot = np.ceil(Ntrain/dimBlock)
+          
+        muZ=np.zeros((Ntrain,nLatentVars))
+           
+        for n_block in np.arange(nBlocksTot):
+            indeces_block = np.arange(n_block*dimBlock,np.amin([(n_block+1)*dimBlock,Ntrain]),dtype=int)
+            aux = trainImages[indeces_block,:]-np.matmul(trainBasisFun[indeces_block,:],np.transpose(W))
+            #muZ[indeces_block,:] = np.linalg.multi_dot((aux,diagBetas,V,SigmaZ))
+            muZ[indeces_block,:] = np.linalg.multi_dot((aux,beta*V,SigmaZ))
+               
+          
+        del aux
+           
+           
+        #compute log marginal likelihood, with Cholesky decomposition
+          
+            
+        U=np.linalg.cholesky(SigmaZ_inv)   #detSigmaZninv=det(U)^2;
+        log_det_SigmaZ_inv=2*np.sum(np.log(np.diag(U)))
+           
+           
+        
+        dimBlock = 1000
+        nBlocksTot = int(np.ceil(Ntrain/dimBlock))
+        sumArray = np.zeros((nBlocksTot,))
+                    
+    
+        for n_block in np.arange(nBlocksTot):
+            indeces_block = np.arange(n_block*dimBlock,np.amin([(n_block+1)*dimBlock,Ntrain]),dtype=int)
+            aux = trainImages[indeces_block,:]-np.matmul(trainBasisFun[indeces_block,:],np.transpose(W))
+            #sum1 = np.trace(np.linalg.multi_dot((aux,diagBetas,np.transpose(aux))))
+            sum1 = np.trace(np.dot(aux,beta*np.transpose(aux)))
+            aux2 = np.dot(aux,beta*V)
+            sum2 = np.trace( np.linalg.multi_dot((aux2,SigmaZ,np.transpose(aux2))))
+            #probably I an use only one multidot 
+            sumArray[n_block] = sum1-sum2
+    
+    
+    
+        del aux
+        summ_logml=np.sum(sumArray)
+            
+        logML = -0.5*(Ntrain*nVoxels*np.log(2*np.pi)-Ntrain*np.sum(np.log(beta))+Ntrain*log_det_SigmaZ_inv+summ_logml)
+          
+        print('logML: ',logML,'\n')
+        
+          
+            
+        #update V
+            
+        A = np.dot(np.transpose(muZ),muZ)+Ntrain*SigmaZ
+        #D = np.dot(np.transpose(trainImages),muZ)-np.linalg.multi_dot((W,np.transpose(trainBasisFun),muZ))
+        #V = np.transpose(np.linalg.solve(np.transpose(A),np.transpose(D)))
+            
+       
+        dimBlock = 20000
+        nBlocksTot = np.ceil(nVoxels/dimBlock)
+        V = np.zeros([nVoxels,nLatentVars])
+        for n_block in np.arange(nBlocksTot):
+            voxelsBlock = np.arange(n_block*dimBlock,np.amin([(n_block+1)*dimBlock,nVoxels]),dtype=int)
+            D_partial=np.dot(np.transpose(trainImages[:,voxelsBlock])-np.dot(W[voxelsBlock,:],np.transpose(trainBasisFun)),muZ)
+            V[voxelsBlock,:] = np.transpose(np.linalg.solve(np.transpose(A),np.transpose(D_partial)))
+            
+        
+        del A
+        del D_partial
+
+        
+        #update beta
+        
+        dimBlock=np.amax([np.floor(1500/(nBasisFun+nLatentVars)),1])
+        #voxel_per_batch=1500
+        nBlocksTot=np.ceil(nVoxels/dimBlock) #N. of batches used for updates during training
+            
+        
+        beta = np.zeros([nVoxels,])
+        for n_block in np.arange(nBlocksTot):
+            voxelsBlock = np.arange(n_block*dimBlock,np.amin([(n_block+1)*dimBlock,nVoxels]),dtype=int)
+                    
+            beta[voxelsBlock] = Ntrain/(
+                 np.transpose(np.sum( np.square(trainImages[:,voxelsBlock]-np.dot(trainBasisFun,np.transpose(W[voxelsBlock,:]))-
+                 np.dot(muZ,np.transpose(V[voxelsBlock,:]))),axis=0))+np.diag(np.linalg.multi_dot((V[voxelsBlock,:],Ntrain*SigmaZ,np.transpose(V[voxelsBlock,:])))))
+                   
+        beta = np.reshape(beta,(nVoxels,1))
+#%%
+
+    nIterations = nIt - 1   
+    timeTraining = time.time() - t
+    print('number of iterations: ', nIterations,'\n')
+    print('training time: ', timeTraining,' s \n')
+        
+    return W, V, beta, timeTraining, logmlVector, nIterations
+
+
+
+
